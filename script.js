4f22832bc4808f99870e1a2f109496e726099cff
Mathias <mathiasnoronha932@gmail.com>
2025-02-06 21:03:33 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara-modal")
+
+function mostrarModal() {
+    modal.style.left = "50%"
+    mascara.style.visibility = "visible"
+}
+function esconderModal() {
+    modal.style.left = "-35%"
+    mascara.style.visibility = "hidden"
+}
